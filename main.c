ee387e140cf53d12b1df45ef110d412a1b5c7fcb
Shail Macwan <119092769+Shail-26@users.noreply.github.com>
2024-10-13 20:03:18 +0530

    Added user input functionality for Sudoku board in grid format

--- main.c
+++ main.c
@@ -1,24 +1,34 @@
 #include "sudoku.h"
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-  Sudoku s = {
-        .board={
-        {'5','3',' ',' ','7',' ',' ',' ',' '},
-        {'6',' ',' ','1','9','5',' ',' ',' '},
-        {' ','9','8',' ',' ',' ',' ','6',' '},
-        {'8',' ',' ',' ','6',' ',' ',' ','3'},
-        {'4',' ',' ','8',' ','3',' ',' ','1'},
-        {'7',' ',' ',' ','2',' ',' ',' ','6'},
-        {' ','6',' ',' ',' ',' ','2','8',' '},
-        {' ',' ',' ','4','1','9',' ',' ','5'},
-        {' ',' ',' ',' ','8',' ',' ','7','9'}
+void inputBoard(Sudoku *s) {
+    printf("Enter your Sudoku puzzle (use 0 or space for empty cells):\n");
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            char input;
+            scanf(" %c", &input);
+            if (input == '0' || input == ' ') {
+                s->board[i][j] = ' '; 
+            } else {
+                s->board[i][j] = input; 
+            }
         }
-    };
-  if(solveSudoku(&s)){
+    }
+}
+
+int main() {
+    Sudoku s;
+
+    inputBoard(&s);
+
+    printf("\n");
+    printf("Sudoku: \n");
+    
+    if (solveSudoku(&s)) {
         printGrid(&s);
-    }else{
+    } else {
         printf("No solution exists.\n");
     }
-  return 0;
+
+    return 0;
 }
