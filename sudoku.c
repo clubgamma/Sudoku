0ed3f4b180834b7048606b64e5d1e08bc4b516b2
Shail Macwan <119092769+Shail-26@users.noreply.github.com>
2024-10-13 15:49:34 +0530

    Completed the incomplete code for solving sudoku

--- sudoku.c
+++ sudoku.c
@@ -1,14 +1,77 @@
 #include<stdio.h>
-#include< "sudoku.h"
+#include "sudoku.h"
 
 int isValid(Sudoku *s,int row,int col,int num){
+    char ch = num + '0'; 
 
+    for (int x = 0; x < SIZE; x++) {
+        if (s->board[row][x] == ch) {
+            return 0; 
+        }
+    }
+
+    for (int x = 0; x < SIZE; x++) {
+        if (s->board[x][col] == ch) {
+            return 0; 
+        }
+    }
+
+    int startRow = row - row % 3, startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (s->board[i + startRow][j + startCol] == ch) {
+                return 0;  
+            }
+        }
+    }
+
+    return 1;
+}
+
+int findEmpty(Sudoku *s, int *row, int *col) {
+    for (*row = 0; *row < SIZE; (*row)++) {
+        for (*col = 0; *col < SIZE; (*col)++) {
+            if (s->board[*row][*col] == ' ') {  
+                return 1;
+            }
+        }
+    }
+    return 0;  
 }
 
 int solveSudoku(Sudoku *s){
+    int row, col;
 
+    if (!findEmpty(s, &row, &col)) {
+        return 1;
+    }
+
+    for (int num = 1; num <= 9; num++) {
+        if (isValid(s, row, col, num)) {
+            s->board[row][col] = num + '0';
+
+            if (solveSudoku(s)) {
+                return 1;
+            }
+
+            s->board[row][col] = ' ';
+        }
+    }
+
+    return 0;
 }
 
 void printGrid(Sudoku *s){
-
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            printf("%c ", s->board[row][col]);
+            if ((col + 1) % 3 == 0 && col < SIZE - 1) {
+                printf("| ");  
+            }
+        }
+        printf("\n");
+        if ((row + 1) % 3 == 0 && row < SIZE - 1) {
+            printf("---------------------\n"); 
+        }
+    }
 }
