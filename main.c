cb5dd2b18f2826ea8e94915ba7f09c8af2e8ac39
git112 <23it021@charusat.edu.in>
2024-10-20 11:49:12 +0530

    Integrated Error Detection

--- main.c
+++ main.c
@@ -1,23 +1,45 @@
 #include "sudoku.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void inputBoard(Sudoku *s) {
-    printf("Enter your Sudoku puzzle (use 0 or space for empty cells):\n");
+    printf("Enter your Sudoku puzzle (use 0 or space for empty cells, and press Enter when a row is done)\n");
     for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            char input = getchar();
+        printf("Row %d :\n", i + 1);
 
-            if (input == '\n') {
-                j--; 
-                continue;
-            }
+        char rowCheck[SIZE + 1] = {0};
+        char line[SIZE + 2];
+        fgets(line, sizeof(line), stdin);
+        line[strcspn(line, "\n")] = 0;
+
+        if (strlen(line) != SIZE) {
+            printf("Error: Each line must contain exactly 9 digits or spaces.\n");
+            exit(1);
+        }
+
+        for (int j = 0; j < SIZE; j++) {
+            char input = line[j];
 
             if (input == '0' || input == ' ') {
-                s->board[i][j] = ' '; 
+                s->board[i][j] = ' ';
+            } else if (input >= '1' && input <= '9') {
+                if (rowCheck[input - '0']) {
+                    printf("Error: Duplicate number");
+                    exit(1);
+                }
+                rowCheck[input - '0'] = 1;
+                s->board[i][j] = input;
             } else {
-                s->board[i][j] = input;  
+                printf("Error: Invalid input. Please enter numbers between 1-9 or 0 or space for empty.\n");
+                exit(1);
             }
         }
+    }
+
+    if (!validateInitialBoard(s)) {
+        printf("Error: Invalid Sudoku puzzle. Conflicts found. Please check your input.\n");
+        exit(1);
     }
 }
 
@@ -33,14 +55,14 @@ void inputBoardFromTxt(Sudoku *s, char *filename) {
             char input = fgetc(file);
 
             if (input == '\n') {
-                j--; 
+                j--;
                 continue;
             }
 
             if (input == '0' || input == ' ') {
-                s->board[i][j] = ' '; 
+                s->board[i][j] = ' ';
             } else {
-                s->board[i][j] = input;  
+                s->board[i][j] = input;
             }
         }
     }
@@ -50,7 +72,7 @@ void inputBoardFromTxt(Sudoku *s, char *filename) {
 
 int main(int argc, char *argv[]) {
     Sudoku s;
-    int steps = 0; 
+    int steps = 0;
 
     if (argc == 2) {
         inputBoardFromTxt(&s, argv[1]);
--- sudoku.c
+++ sudoku.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "sudoku.h"
 
-int isValid(Sudoku *s,int row,int col,int num){
-    char ch = num + '0'; 
+int isValid(Sudoku *s, int row, int col, int num) {
+    char ch = num + '0';
 
     for (int x = 0; x < SIZE; x++) {
         if (s->board[row][x] == ch) {
-            return 0; 
+            return 0;
         }
     }
 
     for (int x = 0; x < SIZE; x++) {
         if (s->board[x][col] == ch) {
-            return 0; 
+            return 0;
         }
     }
 
@@ -20,7 +20,7 @@ int isValid(Sudoku *s,int row,int col,int num){
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (s->board[i + startRow][j + startCol] == ch) {
-                return 0;  
+                return 0;
             }
         }
     }
@@ -31,12 +31,12 @@ int isValid(Sudoku *s,int row,int col,int num){
 int findEmpty(Sudoku *s, int *row, int *col) {
     for (*row = 0; *row < SIZE; (*row)++) {
         for (*col = 0; *col < SIZE; (*col)++) {
-            if (s->board[*row][*col] == ' ') {  
+            if (s->board[*row][*col] == ' ') {
                 return 1;
             }
         }
     }
-    return 0;  
+    return 0;
 }
 
 int solveSudoku(Sudoku *s, int *steps) {
@@ -49,10 +49,10 @@ int solveSudoku(Sudoku *s, int *steps) {
     for (int num = 1; num <= 9; num++) {
         if (isValid(s, row, col, num)) {
             s->board[row][col] = num + '0';
-            (*steps)++;  
+            (*steps)++;
 
             if (solveSudoku(s, steps)) {
-                return 1; 
+                return 1;
             }
 
             s->board[row][col] = ' ';
@@ -62,17 +62,34 @@ int solveSudoku(Sudoku *s, int *steps) {
     return 0;
 }
 
-void printGrid(Sudoku *s){
+void printGrid(Sudoku *s) {
     for (int row = 0; row < SIZE; row++) {
         for (int col = 0; col < SIZE; col++) {
             printf("%c ", s->board[row][col]);
             if ((col + 1) % 3 == 0 && col < SIZE - 1) {
-                printf("| ");  
+                printf("| ");
             }
         }
         printf("\n");
         if ((row + 1) % 3 == 0 && row < SIZE - 1) {
-            printf("---------------------\n"); 
+            printf("---------------------\n");
         }
     }
+}
+
+int validateInitialBoard(Sudoku *s) {
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            if (s->board[row][col] != ' ') {
+                int num = s->board[row][col] - '0';
+                s->board[row][col] = ' ';
+                if (!isValid(s, row, col, num)) {
+                    s->board[row][col] = num + '0';
+                    return 0;
+                }
+                s->board[row][col] = num + '0';
+            }
+        }
+    }
+    return 1;
 }
