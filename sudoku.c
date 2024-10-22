21f2d222a1f9c64e6815ad25227dfa8f41ca0225
nandit27 <nanditkalaria27@gmail.com>
2024-10-22 11:40:37 +0530

    Different Sizes enabled

--- sudoku.c
+++ sudoku.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "sudoku.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h> 
 
-int rowMask[SIZE], colMask[SIZE], boxMask[SIZE];
+int rowMask[MAX_SIZE], colMask[MAX_SIZE], boxMask[MAX_SIZE];
+
+int getBoxIndex(int size, int row, int col) {
+    int boxSize = (int)sqrt(size);
+    return (row / boxSize) * boxSize + (col / boxSize);
+}
 
 int isValid(Sudoku *s, int row, int col, int num) {
     return !(rowMask[row] & (1 << (num - 1)) || 
              colMask[col] & (1 << (num - 1)) || 
-             boxMask[(row / 3) * 3 + (col / 3)] & (1 << (num - 1)));
+             boxMask[getBoxIndex(s->size, row, col)] & (1 << (num - 1)));
 }
 
 void placeOrRemoveNumber(Sudoku *s, int row, int col, int num, int place) {
@@ -14,22 +23,22 @@ void placeOrRemoveNumber(Sudoku *s, int row, int col, int num, int place) {
         s->board[row][col] = num + '0';
         rowMask[row] |= (1 << (num - 1));
         colMask[col] |= (1 << (num - 1));
-        boxMask[(row / 3) * 3 + (col / 3)] |= (1 << (num - 1));
+        boxMask[getBoxIndex(s->size, row, col)] |= (1 << (num - 1));
     } else {
         s->board[row][col] = ' ';
         rowMask[row] &= ~(1 << (num - 1));
         colMask[col] &= ~(1 << (num - 1));
-        boxMask[(row / 3) * 3 + (col / 3)] &= ~(1 << (num - 1));
+        boxMask[getBoxIndex(s->size, row, col)] &= ~(1 << (num - 1));
     }
 }
 
 int solveSudoku(Sudoku *s, int *steps) {
     int row, col;
     // Find an empty cell
-    for (row = 0; row < SIZE; row++) {
-        for (col = 0; col < SIZE; col++) {
+    for (row = 0; row < s->size; row++) {
+        for (col = 0; col < s->size; col++) {
             if (s->board[row][col] == ' ') {
-                for (int num = 1; num <= 9; num++) {
+                for (int num = 1; num <= s->size; num++) {
                     if (isValid(s, row, col, num)) {
                         placeOrRemoveNumber(s, row, col, num, 1); // Place the number
                         (*steps)++;
@@ -47,46 +56,45 @@ int solveSudoku(Sudoku *s, int *steps) {
 }
 
 void printGrid(Sudoku *s) {
-    for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; col++) {
+    for (int row = 0; row < s->size; row++) {
+        for (int col = 0; col < s->size; col++) {
             printf("%c ", s->board[row][col]);
-            if ((col + 1) % 3 == 0 && col < SIZE - 1) {
+            if ((col + 1) % (int)sqrt(s->size) == 0 && col < s->size - 1) {
                 printf("| ");
             }
         }
         printf("\n");
-        if ((row + 1) % 3 == 0 && row < SIZE - 1) {
-            printf("---------------------\n");
+        if ((row + 1) % (int)sqrt(s->size) == 0 && row < s->size - 1) {
+            for (int i = 0; i < s->size + (int)sqrt(s->size) - 1; i++) {
+                printf("-");
+            }
+            printf("\n");
         }
     }
 }
+
 int validateInitialBoard(Sudoku *s) {
-    for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; col++) {
-            if (s->board[row][col] != ' ') {
-                int num = s->board[row][col] - '0';
-                s->board[row][col] = ' ';
-                if (!isValid(s, row, col, num)) {
-                    s->board[row][col] = num + '0';
-                    return 0;
+    memset(rowMask, 0, sizeof(rowMask));
+    memset(colMask, 0, sizeof(colMask));
+    memset(boxMask, 0, sizeof(boxMask));
+
+    for (int i = 0; i < s->size; i++) {
+        for (int j = 0; j < s->size; j++) {
+            if (s->board[i][j] != ' ') {
+                int num = s->board[i][j] - '0';
+                if (!isValid(s, i, j, num)) {
+                    return 0; // Invalid number found
                 }
-                s->board[row][col] = num + '0';
+                placeOrRemoveNumber(s, i, j, num, 1); // Place the number
             }
         }
     }
-    return 1;
+    return 1; // All numbers are valid
 }
 
 const char* classifyDifficulty(int clueCount, int backtrackingSteps) {
-    int score = 40 - clueCount + (backtrackingSteps / 10);
-
-    if (score < 10) {
-        return "Easy";
-    } else if (score >= 10 && score <= 19) {
-        return "Medium";
-    } else if (score >= 20 && score <= 29) {
-        return "Hard";
-    } else {
-        return "Expert";
-    }
+    // Example classification logic
+    if (clueCount > 40 && backtrackingSteps < 50) return "Easy";
+    if (clueCount > 30 && backtrackingSteps < 100) return "Medium";
+    return "Hard";
 }
